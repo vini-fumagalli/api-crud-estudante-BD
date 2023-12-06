cf3523e73fe7271c919851a0ef385bfc6fb05311
fumas <vinifumagalli_@hotmail.com>
2023-12-06 08:24:49 -0300

    style: organização de IStudentService com #region

--- src/Api.Domain/Interfaces/Services/IStudentService.cs
+++ src/Api.Domain/Interfaces/Services/IStudentService.cs
@@ -5,11 +5,16 @@ namespace Api.Domain.Interfaces.Repositories;
 
 public interface IStudentService
 {
+    #region  CRUD 
     Task<ResponseEntity> GetStudents();
     Task<ResponseEntity> GetStudentById(string nameId);
     Task<ResponseEntity> CreateStudent(StudentCreateDto student);
     Task<ResponseEntity> UpdateStudent(string nameId, StudentUpdateDto student);
     Task<ResponseEntity> DeleteStudent(string nameId);
+    #endregion
+
+    #region REGRAS DE NEGÓCIO
     double CalcAverageGrade(double test1Grade, double test2Grade, double projectGrade);
     bool AprovOrNot(double averageGrade);
+    #endregion
 }
